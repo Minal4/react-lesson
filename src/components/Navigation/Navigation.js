3530b14f39c62e706871a263bbf476068fbddfa3
Minal <minal@flipfloptech.com>
2022-04-02 13:24:49 +0545

    Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import './NavigationStyled.css';
+import { FaBars, FaWindowClose } from 'react-icons/fa';
 
 export const Navigation = () => {
+  const [toggle, setToggle] = useState(false);
+  const handleClick = () => {
+    setToggle(~toggle);
+  };
   return (
     <div>
-      <Navbar bg="dark" variant="dark" className="my-3">
+      <Navbar bg="dark" variant="dark" className="mb-3">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <NavLink className="nav-link" to="/">
-              Home
-            </NavLink>
-            <NavLink className="nav-link" to="count">
-              Counter
-            </NavLink>
-            <NavLink className="nav-link" to="tempFunc">
-              Temp Cal
-            </NavLink>
-            <NavLink className="nav-link" to="searchFunc">
-              Search
-            </NavLink>
-            <NavLink className="nav-link" to="quiz">
-              Quiz
-            </NavLink>
-          </Nav>
+          <div>
+            <Nav className={`${toggle ? 'active' : 'navbar'}`}>
+              <NavLink className="nav-link" to="/">
+                Home
+              </NavLink>
+              <NavLink className="nav-link" to="count">
+                Counter
+              </NavLink>
+              <NavLink className="nav-link" to="tempFunc">
+                Temp Cal
+              </NavLink>
+              <NavLink className="nav-link" to="searchFunc">
+                Search
+              </NavLink>
+              <NavLink className="nav-link" to="quiz">
+                Quiz
+              </NavLink>
+            </Nav>
+            <div className="icon" onClick={handleClick}>
+              {toggle ? <FaWindowClose /> : <FaBars />}
+            </div>
+          </div>
         </Container>
       </Navbar>
     </div>
